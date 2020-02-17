d924e77637f3e8bb3525e323ea8d9e2dedf534aa
ArzamasRU <ArzamasRU@gmail.com>
2020-02-17 18:12:11 +0300

    DISTRIBUT CODE BY MULTIPLE CLASSES

---
+++ src/main/java/ru/lavrov/tm/Project.java
@@ -0,0 +1,17 @@
+package ru.lavrov.tm;
+
+public class Project {
+    String name;
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/lavrov/tm/Task.java
@@ -0,0 +1,17 @@
+package ru.lavrov.tm;
+
+public class Task {
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
