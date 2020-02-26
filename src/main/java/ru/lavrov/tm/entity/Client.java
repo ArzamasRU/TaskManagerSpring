03b777105e65f2b2bde5bdd0b26cffcb0623bb96
ArzamasRU <ArzamasRU@gmail.com>
2020-02-26 19:46:44 +0300

    add client

---
+++ src/main/java/ru/lavrov/tm/entity/Client.java
@@ -0,0 +1,33 @@
+package ru.lavrov.tm.entity;
+
+import ru.lavrov.tm.role.Role;
+
+public class Client {
+    String login;
+    String password;
+    Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
